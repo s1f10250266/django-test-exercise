1aef67acc351597fe3239e5b6f63676a32d83973
s1f10250266 <s1f102502665@iniad.org>
2026-07-02 15:05:25 +0900

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -33,6 +33,13 @@ def detail(request, task_id):
     return render(request, 'todo/detail.html', context)
 
 def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.delete()
+    return redirect(index)
+    
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
