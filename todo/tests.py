cd35dcc31ae1c7224e847517e5475f08c5ddb41e
1f10250145 <s1f10250145@iniad.org>
2026-07-09 11:43:56 +0900

    更新機能のテストの追加

--- todo/tests.py
+++ todo/tests.py
@@ -105,6 +105,30 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/detail.html')
         self.assertEqual(response.context['task'], task)
 
+    def test_update_get_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        response = client.get('/{}/update'.format(task.pk))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/edit.html')
+        self.assertEqual(response.context['task'], task)
+
+    def test_update_post_success(self):
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task.save()
+        client = Client()
+        data = {'title': 'Updated Task', 'due_at': '2024-08-01 12:00:00'}
+        response = client.post('/{}/update'.format(task.pk), data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/{}/'.format(task.pk))
+
+        updated_task = Task.objects.get(pk=task.pk)
+        self.assertEqual(updated_task.title, 'Updated Task')
+        self.assertEqual(updated_task.due_at, timezone.make_aware(datetime(2024, 8, 1, 12, 0, 0)))
+
     def test_detail_get_fail(self):
         client = Client()
         response = client.get('/1/')
